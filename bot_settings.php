2fce6fb20b0c11c6a277246c241545413f0f1054
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-01-29 17:39:29 +0700

    Add files via upload

---
+++ bot_settings.php
@@ -0,0 +1,8 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1558893178');
+define('LINE_MESSAGE_CHANNEL_SECRET','a1e4e88d6473b48c6d2692042a98c15e
+');
+define('LINE_MESSAGE_ACCESS_TOKEN','9B9ffZ7XJ/iWMWgJuqRV/oaVVMfELLEMmBjoIhqG9E5xdFvOHDNpiZBDjdi2deqYm4SdFCezBQjddXs1EgjLXmCJgBorihv3bfwUxW8zMCoT9EqBEs5CW6wnsUqEoJcKTGPyYznGlnG293DicIlZowdB04t89/1O/w1cDnyilFU=
+');
+?>
